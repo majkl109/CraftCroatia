03be3cfced73c32af6c8a6ad284915ab9a2365b6
Majkl <majkl.andracic@gmail.com>
2020-03-06 13:53:52 +0100

    BeerAdapter created

--- app/src/main/java/com/majkl/craftcroatia/BeerAdapter.java
+++ app/src/main/java/com/majkl/craftcroatia/BeerAdapter.java
@@ -3,12 +3,15 @@ package com.majkl.craftcroatia;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 import java.util.List;
+
 
 public class BeerAdapter extends RecyclerView.Adapter<BeerAdapter.ViewHolder> {
 
@@ -18,8 +21,8 @@ public class BeerAdapter extends RecyclerView.Adapter<BeerAdapter.ViewHolder> {
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-       View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_beer,parent,false);
-       return  new ViewHolder(view);
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_beer, parent, false);
+        return new ViewHolder(view);
     }
 
     @Override
@@ -28,10 +31,49 @@ public class BeerAdapter extends RecyclerView.Adapter<BeerAdapter.ViewHolder> {
 
     }
 
-
-
     @Override
     public int getItemCount() {
-        return 0;
+        return data.size();
+    }
+
+    public void setData(ArrayList<BeerData> data) {
+        this.data = new ArrayList<>();
+        this.data.addAll(data);
+        notifyDataSetChanged();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+
+        private TextView title;
+        private TextView address;
+        private ImageView drawable;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(ItemView);
+            itemView.setOnClickListener(this);
+            title = itemView.findViewById(R.id.title);
+            address = itemView.findViewById(R.id.address);
+            drawable = itemView.findViewById(R.id.image);
+        }
+
+        public void bindData(BeerData beerData) {
+            title.setText(beerData.getTitle());
+            address.setText(beerData.getAddress());
+            drawable.setImageResource(beerData.getDrawable());
+        }
+
+        @Override
+        public void onClick(View view) {
+            listener.onItemClicked(data.get(getAdapterPosition()), getAdapterPosition());
+
+        }
+    }
+
+    public interface IOnItemClicked {
+        void onItemClicked(BeerData item, int position);
+    }
+
+    public void setListener(IOnItemClicked listener) {
+        this.listener = listener;
     }
 }
