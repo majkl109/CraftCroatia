17c9adf4052d349011878e3cd24c388fa0c113b7
Majkl <majkl.andracic@gmail.com>
2020-03-10 21:54:54 +0100

    Beer list update

--- app/src/main/java/com/majkl/craftcroatia/MainActivity.java
+++ app/src/main/java/com/majkl/craftcroatia/MainActivity.java
@@ -31,12 +31,17 @@ public class MainActivity extends AppCompatActivity implements BeerAdapter.IOnIt
 
         ArrayList<BeerData> data = new ArrayList<>();
 
-        data.add(new BeerData("Neposlusna", "Zadar", R.drawable.neposlusna));
-        data.add(new BeerData("BuraBrew", "Zadar", R.drawable.bura_brew));
+        data.add(new BeerData("Neposlušna", "Zadar", R.drawable.neposlusna));
+        data.add(new BeerData("BuraBrew", "Poreč", R.drawable.bura_brew));
         data.add(new BeerData("Lepi decki", "Zagreb", R.drawable.lepi_decki_waitapu));
         data.add(new BeerData("Nova runda", "Zagreb", R.drawable.nova_runda));
-        data.add(new BeerData("Zmajsko", "Zagreb", R.drawable.zmajsko));
-
+        data.add(new BeerData("Zmajska", "Zagreb", R.drawable.zmajsko));
+        data.add(new BeerData("Barba", "Split", R.drawable.barba));
+        data.add(new BeerData("Medvedgrad", "Zagreb", R.drawable.medvedgrad));
+        data.add(new BeerData("Varionica", "Zagreb", R.drawable.varionica));
+        data.add(new BeerData("Visibaba", "Zagreb", R.drawable.visibaba));
+        data.add(new BeerData("Garden", "Zagreb", R.drawable.garden));
+        data.add(new BeerData("Crafter's", "Leprovice", R.drawable.crafters));
 
         return  data;
     }
